5fb288971ad38d8518038a27c154d5042a5e36f1
Damian Birchler <damian.birchler@ost.ch>
2024-05-05 17:23:11 +0200

    Playing around. Match variable names.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,67 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod parse {
+    use regex;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    struct Tokens {
+        variable: regex::Regex,
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    impl Tokens {
+        pub fn new() -> Tokens {
+            Tokens {
+                variable: regex::Regex::new(r"[A-Za-z]+").unwrap(),
+            }
+        }
+    }
+
+    #[derive(PartialEq, Debug)]
+    pub struct Variable<'i> {
+        name: &'i str,
+    }
+
+    #[derive(PartialEq, Debug)]
+    pub struct Error<'i> {
+        message: &'i str,
+    }
+
+    fn parse_variable<'i, 'e>(tokens: Tokens, input: &'i str) -> Result<Variable<'i>, Error<'e>> {
+        match tokens.variable.find(&input) {
+            Some(m) => Ok(Variable { name: m.as_str() }),
+            None => Err(Error {
+                message: "Not a variable.",
+            }),
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_a_variable() {
+            let tokens = Tokens::new();
+
+            let result = parse_variable(tokens, "VIceSscaled");
+
+            assert_eq!(
+                result,
+                Ok(Variable {
+                    name: "VIceSscaled"
+                })
+            );
+        }
+
+        #[test]
+        fn test_not_a_variable() {
+            let tokens = Tokens::new();
+
+            let result = parse_variable(tokens, "1.297");
+
+            assert_eq!(
+                result,
+                Err(Error {
+                    message: "Not a variable."
+                })
+            );
+        }
     }
 }
